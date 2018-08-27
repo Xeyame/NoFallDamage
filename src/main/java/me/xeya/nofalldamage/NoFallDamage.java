dd714d379d90aaf9575a3c10c556e1cec54e295e
Xeyame <34656654+Xeyame@users.noreply.github.com>
2018-08-27 23:22:30 +0200

    Initial upload

---
+++ src/main/java/me/xeya/nofalldamage/NoFallDamage.java
@@ -0,0 +1,24 @@
+package me.xeya.nofalldamage;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoFallDamage extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            if (event.getCause() == EntityDamageEvent.DamageCause.FALL) {
+                event.setCancelled(true);
+            }
+        }
+    }
+}
